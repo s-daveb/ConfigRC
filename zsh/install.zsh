7b3990989dd594d7d3af500fc63d42ccc5c9fb1b
S David <2100425+s-daveb@users.noreply.github.com>
2022-11-10 19:18:09 -0500

    Tweaked installer and zshenv to account for new git submodule
    Now linking the zplug repository to .local/share/zplug

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -3,17 +3,26 @@
 # Script working directory
 SWD="$(dirname $0)"
 
+ZPLUG_LINK_DEST="${HOME}/.local/share"
+
 cd $SWD
 REPODIR=$PWD
 
 cd $HOME
 
+# Clear out old config
 unlink "${HOME}/.zshrc"
 unlink "${HOME}/.zshenv"
-unlink -R "${HOME}/.zshenv.d" 2> /dev/null
-unlink -R "${HOME}/.zshrc.d" 2> /dev/null
 
-ln -sv "${REPODIR}/zshrc" "${HOME}/.zshrc"
+unlink "${HOME}/.zshenv.d" 2> /dev/null
+unlink "${HOME}/.zshrc.d" 2> /dev/null
+unlink "${ZPLUG_LINK_DEST}/zplug" 2> /dev/null
+
+# Link zplug
+
+[ ! -d "${ZPLUG_LINK_DEST}" ] && mkdir -pv "${ZPLUG_LINK_DEST}"
+ln -sv "${REPODIR}/dependencies/zplug" "${ZPLUG_LINK_DEST}"
+
 ln -sv "${REPODIR}/zshenv" "${HOME}/.zshenv"
+ln -sv "${REPODIR}/zshrc" "${HOME}/.zshrc"
 ln -sv "${REPODIR}/zshrc.d" "${HOME}/.zshrc.d"
-
