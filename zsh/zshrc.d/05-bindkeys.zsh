16499529eaafc5f3223fa334d8e7bfa5a5956f5a
S David <2100425+s-daveb@users.noreply.github.com>
2022-12-27 13:31:04 -0500

    Adding zsh configuration as well as bindings for home/end/Ctrl+left/Ctrl+right

--- zsh/zshrc.d/05-bindkeys.zsh
+++ zsh/zshrc.d/05-bindkeys.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[F" end-of-line
+bindkey '\e[H'    beginning-of-line
+bindkey '\e[F'    end-of-line
+
+bindkey '[1;5C' forward-word
+bindkey '[1;5D' backward-word
