136191dded5f1d2882eef013e4cf637905ddac54
S David <2100425+s-daveb@users.noreply.github.com>
2023-01-09 00:26:18 -0500

    zsh: export Qt5_DIR path from brew prefix

--- zsh/zshrc.d/10-Qt5.zsh
+++ zsh/zshrc.d/10-Qt5.zsh
@@ -1,5 +1,7 @@
 
-PKG_CONFIG_PATH+="/usr/local/opt/qt5/lib/pkgconfig"
+BREW_PREFIX="$(brew --prefix)"
+PKG_CONFIG_PATH+="/${BREW_PREIFX}/opt/qt5/lib/pkgconfig"
 export PKG_CONFIG_PATH
 
-export PATH="/usr/local/opt/qt5/bin:$PATH"
+export PATH="${BREW_PREIFX}/opt/qt5/bin:$PATH"
+export Qt5_DIR="${BREW_PREIFX}/opt/qt5/"
