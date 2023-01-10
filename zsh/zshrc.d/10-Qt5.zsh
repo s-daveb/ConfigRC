8a9b3d015fd0e398d897e8e20145811e3626c184
S David <2100425+s-daveb@users.noreply.github.com>
2023-01-09 20:12:45 -0500

    (zshrc.d) prepare Qt5 for transition to M1 mac. refactored variables.

--- zsh/zshrc.d/10-Qt5.zsh
+++ zsh/zshrc.d/10-Qt5.zsh
@@ -1,7 +1,7 @@
 
 BREW_PREFIX="$(brew --prefix)"
-PKG_CONFIG_PATH+="/${BREW_PREIFX}/opt/qt5/lib/pkgconfig"
+PKG_CONFIG_PATH+="${BREW_PREFIX}/opt/qt5/lib/pkgconfig"
 export PKG_CONFIG_PATH
 
-export PATH="${BREW_PREIFX}/opt/qt5/bin:$PATH"
-export Qt5_DIR="${BREW_PREIFX}/opt/qt5/"
+export PATH="${BREW_PREFIX}/opt/qt5/bin:$PATH"
+export Qt5_DIR="${BREW_PREFIX}/opt/qt5/"
