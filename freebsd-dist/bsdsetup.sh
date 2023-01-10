03f1ec6c04dec6fac0453a7a3b58ff7e04b745a0
S David <2100425+s-daveb@users.noreply.github.com>
2023-01-10 14:58:12 -0500

    (freebsd-dist) Untested push of basic installation code. Will test ASAP.

--- freebsd-dist/bsdsetup.sh
+++ freebsd-dist/bsdsetup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 pkg upgrade -y pkg
-pkg install -y bash zsh vim doas mosh tmux
+pkg install -y bash zsh vim doas mosh tmux git python3
+
+# Get iocage
+exact_iocage_pkg="$(pkg search -x "py([0123456789]{2,3})-iocage-([0123456789])(.*)" | awk '{ print $1 }')"
+
+pkg install -y ${exact_iocage_pkg}
