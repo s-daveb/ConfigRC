8f91bc7438db1725a36246875068a26a8748dbc9
S David <2100425+s-daveb@users.noreply.github.com>
2023-02-02 13:04:26 -0500

    zsh: fix ruby paths, so that /bin is appended to each path

--- zsh/zshrc.d/03-path-ruby-3.0.0.zsh
+++ zsh/zshrc.d/03-path-ruby-3.0.0.zsh
@@ -1,6 +1,12 @@
 
 GEM_BIN=`which gem`
 
+
 if [ -x "${GEM_BIN}" ]; then
-	export PATH="${PATH}:$(${GEM_BIN} environment path)"
+	RUBYPATHS=$(${GEM_BIN} environment path | sed 's/:/\n/g')
+
+	for rubypath in ${=RUBYPATHS}; do
+		export PATH="${PATH}:${rubypath}/bin"
+	done
 fi
+
