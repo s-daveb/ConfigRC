332586f8e2e3b1089a92210419224a6abb3c366b
S David <2100425+s-daveb@users.noreply.github.com>
2023-05-03 11:38:32 -0400

    Adding conditional alias for ls -> colorls, with theme awareness

--- zsh/zshrc.d/98-aliases.zsh
+++ zsh/zshrc.d/98-aliases.zsh
@@ -1,11 +1,37 @@
+alias bstart="brew services start"
+alias bstop="brew services stop"
 
-function doas() {
-	sudo_args=""
-	if [ "$1" = "-S" ] || [ "$1" = "-s" ]; then
-		sudo_args="-i"
-		shift
+alias brestart="brew services restart"
+
+function colorls_cmd_generator()
+{
+	color_param="--light"
+
+	# colorls should only be in light mode if the session is not
+	# coming through an iTerm tmux session
+	if [[ "${TERM_PROGRAM}" == "tmux"  ]] || [[ $(date +"%H") -gt 17 ]]; then
+		color_param="--dark"
 	fi
-	sudo ${sudo_args} ${@}
+
+	echo "colorls --gs ${color_param}"
 }
 
-alias usystemctl="systemctl --user" 
+function alias_colorls()
+{
+	colorls 2> /dev/null 1> /dev/null
+	colorls_result=$?
+
+	colorls_cmd="$(colorls_cmd_generator)"
+	if [ $colorls_result -eq 0 ]; then
+		alias ls="${colorls_cmd}"
+	else
+		alias ls="ls --color=auto"
+	fi
+}
+
+alias_colorls
+
+alias usystemctl="systemctl --user"
+alias docker="sudo docker"
+alias podman="sudo podman"
+alias docker-compose="sudo docker-compose"
---
+++ zsh/zshrc.d/98.2-docker-aliases.zsh
@@ -0,0 +1,4 @@
+alias usystemctl="systemctl --user"
+alias docker="sudo docker"
+alias podman="sudo podman"
+alias docker-compose="sudo docker-compose"
