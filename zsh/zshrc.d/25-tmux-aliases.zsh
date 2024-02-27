2762322b9682438a5406ef2c5acd24ebfe85ba0a
S David <2100425+s-daveb@users.noreply.github.com>
2024-02-27 11:42:11 -0500

    Add terminfo file for tmux and add tmuxcmd alias that executes commands on current tmux socket

--- zsh/zshrc.d/25-tmux-aliases.zsh
+++ zsh/zshrc.d/25-tmux-aliases.zsh
@@ -1,3 +1,26 @@
+
+unalias tmux 2> /dev/null
 tmux_path=$(which tmux)
-alias tmux="${tmux_path} -L standard"
-alias tsession="${tmux_path} -L standard new-session -As"
+
+if [ -n "$tmux_path" ]; then
+	function get_tmux_socket()
+	{
+		echo "${TMUX}" | sed 's/,.*//'
+	}
+
+	function tmux_with_current_socket()
+	{
+		tmux_socket=`get_tmux_socket`
+		to_run=""
+		if [ -n "${tmux_socket}" ]; then
+			to_run="${tmux_path} -S ${tmux_socket}  ${@}"
+		else
+			to_run="${tmux_path} ${@}"
+		fi
+
+		echo  "running '$to_run'"
+		eval $to_run
+	}
+
+	alias tmuxcmd="tmux_with_current_socket"
+fi
---
+++ zsh/zshrc.d/26-terminfo.zsh
@@ -0,0 +1,1 @@
+export TERMINFO_DIRS=$TERMINFO_DIRS:$HOME/.local/share/terminfo
