e861572b82e9fddb7877225a1b837e1b1a0b2e71
S David <2100425+s-daveb@users.noreply.github.com>
2024-05-01 10:29:51 -0400

    [vim] add missing init.vim file

--- vim/install.sh
+++ vim/install.sh
@@ -2,8 +2,13 @@
 
 # Script working directory
 SWD="$(dirname $0)"
+VIMRC="${HOME}/.vimrc"
 PLUGDIR="${HOME}/.vim-plug"
 SWAPDIR="${HOME}/.cache/vim"
+
+NVIM_CONFDIR="${HOME}/.config/nvim"
+NVIMRC="${NVIM_CONFDIR}/init.vim"
+NVIM_SWAPDIR="`echo ${SWAPDIR} | sed 's/vim/nvim/g'`"
 
 cd $SWD
 REPODIR=$PWD
@@ -17,6 +22,10 @@ cmd_echo() {
 [ ! -d "${SWAPDIR}/undo" ]  && mkdir -p "${SWAPDIR}/undo"
 [ ! -d "${SWAPDIR}/view" ]  && mkdir -p "${SWAPDIR}/view"
 
+[ ! -d "${NVIM_SWAPDIR}/swap" ]  && mkdir -p "${NVIM_SWAPDIR}/swap"
+[ ! -d "${NVIM_SWAPDIR}/undo" ]  && mkdir -p "${NVIM_SWAPDIR}/undo"
+[ ! -d "${NVIM_SWAPDIR}/view" ]  && mkdir -p "${NVIM_SWAPDIR}/view"
+
 [ ! -d $PLUGDIR ]  && mkdir -p $PLUGDIR
 
 if [ -L "${HOME}/.vim" ]; then
@@ -25,13 +34,20 @@ else
     mv -v "${HOME}/.vim" "${HOME}/vimconfig.old"
 fi
 
-if [ -L "${HOME}/.vimrc" ]; then
-	cmd_echo unlink "${HOME}/.vimrc"
+if [ -L "${VIMRC}" ]; then
+	cmd_echo unlink "${VIMRC}"
 else
-    mv -v "${HOME}/.vimrc" "${HOME}/vimrc.old"
+    mv -v "${VIMRC}" "${HOME}/vimrc.old"
 fi
 
-ln -sv "${REPODIR}/vimrc"    "$HOME/.vimrc"
-ln -sv "${REPODIR}/vimfiles" "$HOME/.vim"
+if [ -L "${NVIMRC}" ]; then
+	cmd_echo unlink "${NVIMRC}"
+else
+    mv -v "${NVIMRC}" "${HOME}/init.vim.old"
+fi
+
+ln -sv "${REPODIR}/vimrc"     "${VIMRC}"
+ln -sv "${REPODIR}/init.nvim" "${NVIMRC}"
+ln -sv "${REPODIR}/vimfiles"  "${HOME}/.vim"
 
 vim +PlugInstall +qa
