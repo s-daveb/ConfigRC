3ac39f1aa2a13ebe2cc63ff750ea77ae49ed122c
S David <2100425+s-daveb@users.noreply.github.com>
2024-12-13 20:38:15 -0500

    [nvim] Adding .{cpu_arch} prefix to default cmake build path in neovim-tasks

--- vim/nvimfiles/lua/config/neovim-tasks.lua
+++ vim/nvimfiles/lua/config/neovim-tasks.lua
@@ -3,6 +3,7 @@ local tasks = require('tasks')
 local subcommands = require('tasks.subcommands')
 local dapui = require('dapui')
 
+
 local M = {}
 
 local function setup_commands()
@@ -66,11 +67,12 @@ end
 
 function M.setup()
     local Path = require('plenary.path')
+    local cpu_arch = vim.loop.os_uname().machine
     tasks.setup({
         default_params = { -- Default module parameters with which `neovim.json` will be created.
             cmake = {
                 cmd = 'cmake', -- CMake executable to use, can be changed using `:Task set_module_param cmake cmd`.
-                build_dir = tostring(Path:new('{cwd}', 'build', '{build_type}')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
+                build_dir = tostring(Path:new('{cwd}', 'build', '{build_type}.' .. cpu_arch  )), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
                 build_type = 'Debug', -- Build type, can be changed using `:Task set_module_param cmake build_type`.
                 dap_name = 'lldb',
                 args = { -- Task default arguments.
