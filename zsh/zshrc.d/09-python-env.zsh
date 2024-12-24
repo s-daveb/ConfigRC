59e0d2c72730ac1dad604d9ec4d962a456f1d4cd
S David <2100425+s-daveb@users.noreply.github.com>
2024-12-23 22:51:52 -0500

    [zsh] switch to python venv instead of pyenv

--- zsh/zshrc.d/09-python-env.zsh
+++ zsh/zshrc.d/09-python-env.zsh
@@ -1,7 +1,22 @@
 
-local python_venv_path="${HOME}/.local/python/"
-alias venv-activate="source ${python_venv_path}/bin/activate"
 
-export PYENV_ROOT="$HOME/.pyenv"
-eval "$(pyenv init -)"
+VIRTUAL_ENV="$HOME/.venv"
+reinstall=0
+
+if [ ! -d $VIRTUAL_ENV ]; then
+    mkdir -p $VIRTUAL_ENV
+    python3 -m venv $VIRTUAL_ENV
+    reinstall=1
+fi
+
+if [ -d $VIRTUAL_ENV ]; then
+    export VIRTUAL_ENV
+    export PATH="$VIRTUAL_ENV/bin:$PATH"
+fi
+
+if [ $reinstall -eq 1 ]; then
+    pip install --upgrade pip
+    pip install --upgrade setuptools
+    pip install --upgrade wheel
+fi
 
