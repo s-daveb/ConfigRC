1fc111bcd5dfedc8c6af79ab6a24f14120345d21
S David <2100425+s-daveb@users.noreply.github.com>
2025-05-12 11:08:32 -0400

    Minor fixes to zshrc, removing unneded spaces and minor MANPATH syntax issues

--- zsh/zshrc.d/05-manpath.zsh
+++ zsh/zshrc.d/05-manpath.zsh
@@ -2,4 +2,9 @@
 if [ -x "$(which gman)" ]; then
 	alias man="$(which gman)"
 fi
-export MANPATH=:${HOME}/.cache/cppman/cppreference.com
+
+if [ -n "$MANPATH" ]; then
+	export MANPATH=${MANPATH}:${HOME}/.cache/cppman/cppreference.com
+else
+	export MANPATH=${HOME}/.cache/cppman/cppreference.com
+fi
