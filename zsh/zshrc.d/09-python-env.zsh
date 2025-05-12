ba2e97f67edd2184edcf83a79365ee61a1131be8
S David <2100425+s-daveb@users.noreply.github.com>
2025-05-12 11:00:37 -0400

    Fix python and micromamba env setup; drop down to /bin/sh to use micromamba; zshrc breaks it

--- zsh/zshrc.d/09-python-env.zsh
+++ zsh/zshrc.d/09-python-env.zsh
@@ -1,39 +1,48 @@
-## >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba shell init' !!
-export MAMBA_EXE='/opt/homebrew/opt/micromamba/bin/mamba';
-export MAMBA_ROOT_PREFIX='/Users/sdavid/.mamba';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
-fi
-unset __mamba_setup
-# <<< mamba initialize <<<
+VIRTUAL_ENV="$HOME/.venv"
+reinstall=0
 
-# Skip .venv setup if conda is already in use
-if [ -z "$MAMBA_ROOT_PREFIX" ]; then
-	VIRTUAL_ENV="$HOME/.venv"
-	reinstall=0
-
-	if [ ! -d "$VIRTUAL_ENV" ]; then
-		mkdir -p "$VIRTUAL_ENV"
-		python3 -m venv "$VIRTUAL_ENV"
-		reinstall=1
-	fi
-
-	export VIRTUAL_ENV
-	export PATH="$VIRTUAL_ENV/bin:$PATH"
-
-	if [ $reinstall -eq 1 ]; then
-		pip install --upgrade pip setuptools wheel
-	fi
-else
-	# Conda init (only if installed)
-	if [ -x "/opt/homebrew/Caskroom/miniconda/base/bin/conda" ]; then
-		eval "$(/opt/homebrew/Caskroom/miniconda/base/bin/conda shell.zsh hook)"
-	fi
+if [ ! -d "$VIRTUAL_ENV" ]; then
+	mkdir -p "$VIRTUAL_ENV"
+	python3 -m venv "$VIRTUAL_ENV"
+	reinstall=1
 fi
 
+export VIRTUAL_ENV
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+
+if [ $reinstall -eq 1 ]; then
+	pip install --upgrade pip setuptools wheel
+fi
+
+function micromamba_init()
+{
+	shell="${1:-dash}"
+
+	case "$shell" in
+		dash)
+			# Code for dash shell
+			env -i PATH="$PATH" HOME="$HOME" TERM="$TERM" /bin/dash
+			;;
+		zsh)
+			# Code for zsh shell
+			env -i PATH="$PATH" HOME="$HOME" TERM="$TERM" /bin/zsh --no-rcs -l
+			;;
+		*)
+			case "$SHELL" in
+				dash)
+					# Code for bash shell
+					env -i PATH="$PATH" HOME="$HOME" TERM="$TERM" /bin/dash -l
+					;;
+				zsh)
+					# Code for zsh shell
+					env -i PATH="$PATH" HOME="$HOME" TERM="$TERM" /bin/zsh --no-rc -l
+					;;
+				*)
+					echo "using unsupported shell" 1>&2
+					return
+					;;
+			esac
+	esac
+}
 
 # vim: set ts=4 sts=4 noet sw=4 :
