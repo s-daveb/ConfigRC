29979896dd6e6786cbc6263ec8e1475678a66598
S David <2100425+s-daveb@users.noreply.github.com>
2025-05-24 09:45:59 -0400

    Revert changes to manpath. I should have read the manual lol

--- zsh/zshrc.d/05-manpath.zsh
+++ zsh/zshrc.d/05-manpath.zsh
@@ -3,8 +3,4 @@ if [ -x "$(which gman)" ]; then
 	alias man="$(which gman)"
 fi
 
-if [ -n "$MANPATH" ]; then
-	export MANPATH=${MANPATH}:${HOME}/.cache/cppman/cppreference.com
-else
-	export MANPATH=${HOME}/.cache/cppman/cppreference.com
-fi
+export MANPATH=:${HOME}/.cache/cppman/cppreference.com
