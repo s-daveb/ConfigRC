d3a1e8628b6eccd8df8c5e253f8aa7d7c1637e10
S David <2100425+s-daveb@users.noreply.github.com>
2025-05-24 09:47:52 -0400

    [nvim.lsp.python] add local venv path from ~/.venv/.bin to $PATH before checking for pylsp

--- vim/nvimfiles/lua/config/lsp/providers/python.lua
+++ vim/nvimfiles/lua/config/lsp/providers/python.lua
@@ -1,10 +1,21 @@
 local M = {}
 
-local vim = vim
 local lspconfig
 local capabilities
 local keymapper
 
+local function load_home_venv()
+	local uv = vim.loop
+	local venv_path = os.getenv("HOME") .. "/.venv/bin"
+
+	-- Check if the directory exists
+	local stat = uv.fs_stat(venv_path)
+	if stat and stat.type == "directory" then
+		vim.env.PATH = venv_path .. ":" .. vim.env.PATH
+	end
+end
+
+load_home_venv()
 local pyls_path = vim.fn.exepath('pylsp')
 
 function M.setup(opts)
@@ -15,6 +26,7 @@ function M.setup(opts)
 	if vim.fn.executable(pyls_path) == 1 then
 		lspconfig.pylsp.setup {
 			cmd = { pyls_path },
+			filetypes = { 'python' },
 			--filetypes = { 'c', 'cpp', 'c.doxygen', 'cpp.doxygen', 'objc', 'objcpp' },
 			on_attach = keymapper.set_keys,
 			capabilities = capabilities
